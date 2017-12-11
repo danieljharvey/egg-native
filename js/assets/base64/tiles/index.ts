0ac17c64225eca1ea382af48ba86464100636b2c
Daniel Harvey <danieljamesharvey@gmail.com>
2017-12-11 22:44:36 +0000

    Load tiles, setup prettier

---
+++ js/assets/base64/tiles/index.ts
@@ -0,0 +1,49 @@
+import { bladeEggCup } from "./blade-egg-cup";
+import { cacti } from "./cacti";
+import { crate } from "./crate";
+import { door } from "./door";
+import { eggCup } from "./egg-cup";
+import { eggSplitter } from "./egg-splitter";
+import { fabric } from "./fabric";
+import { findBladeEggCup } from "./find-blade-egg-cup";
+import { greenDoor } from "./green-door";
+import { greenDoorOpen } from "./green-door-open";
+import { greenSwitch } from "./green-switch";
+import { mud } from "./mud";
+import { pinkDoor } from "./pink-door";
+import { pinkDoorOpen } from "./pink-door-open";
+import { pinkSwitch } from "./pink-switch";
+import { plant } from "./plant";
+import { silverEggCup } from "./silver-egg-cup";
+import { sky } from "./sky";
+import { tile } from "./tile";
+import { toast } from "./toast";
+import { workSurface2 } from "./work-surface-2";
+import { workSurface3 } from "./work-surface-3";
+import { workSurface4 } from "./work-surface-4";
+
+export const tileImages = {
+  "blade-egg-cup": bladeEggCup,
+  cacti: cacti,
+  crate: crate,
+  door: door,
+  "egg-cup": eggCup,
+  "egg-splitter": eggSplitter,
+  fabric: fabric,
+  "find-blade-egg-cup": findBladeEggCup,
+  "green-door": greenDoor,
+  "green-door-open": greenDoorOpen,
+  "green-switch": greenSwitch,
+  mud: mud,
+  "pink-door": pinkDoor,
+  "pink-door-open": pinkDoorOpen,
+  "pink-switch": pinkSwitch,
+  plant: plant,
+  "silver-egg-cup": silverEggCup,
+  sky: sky,
+  tile: tile,
+  toast: toast,
+  "work-surface-2": workSurface2,
+  "work-surface-3": workSurface3,
+  "work-surface-4": workSurface4
+};

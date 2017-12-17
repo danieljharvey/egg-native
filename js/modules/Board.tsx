dd78a0dcb34ab8673edbcea8c89a7086a3f95591
Daniel Harvey <danieljamesharvey@gmail.com>
2017-12-17 22:54:04 +0000

    Change background color

--- js/modules/Board.tsx
+++ js/modules/Board.tsx
@@ -224,7 +224,7 @@ export default class BoardComponent extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center"
   },

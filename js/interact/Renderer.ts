4487cbcdbfce01347a0a2a9967360f2fe81ab9e7
Daniel Harvey <danieljamesharvey@gmail.com>
2017-12-17 22:30:09 +0000

    basic egg movement

--- js/interact/Renderer.ts
+++ js/interact/Renderer.ts
@@ -139,7 +139,6 @@ export class Renderer {
     for (const i in this.playerTypes) {
       if (this.playerTypes[i] !== undefined) {
         const playerType = this.playerTypes[i];
-
         const imageName = playerType.img;
 
         const base64 = playerImages[imageName];
@@ -187,7 +186,6 @@ export class Renderer {
     ctx.globalAlpha = 1;
     const tiles = board.getAllTiles();
     tiles.map(tile => {
-      console.log(tile)
       const needsDraw = renderMap[tile.x][tile.y];
       if (needsDraw === false) {
         this.showUnrenderedTile(tile.x, tile.y);
@@ -319,10 +317,10 @@ export class Renderer {
 
     const left = Math.floor(coords.x * tileSize + coords.offsetX * offsetRatio);
     const top = Math.floor(coords.y * tileSize + coords.offsetY * offsetRatio);
-
+    
     const clipLeft = Math.floor(player.currentFrame * SPRITE_SIZE);
     const clipTop = 0;
-
+    
     const image = this.getPlayerImage(player.img);
     if (!image) {
       // console.log('player image not loaded', player.img);
--- js/modules/Board.tsx
+++ js/modules/Board.tsx
@@ -4,10 +4,15 @@ import Canvas, { Image } from "react-native-canvas";
 
 import CanvasClass from "../interact/Canvas";
 import { Renderer } from "../interact/Renderer";
-import { PlayerTypes } from "../logic/PlayerTypes";
+import { playerTypes } from "../logic/PlayerTypes";
 import { Board } from "../objects/Board";
 import { BoardSize } from "../objects/BoardSize";
+import { Coords } from "../objects/Coords";
+import { GameState } from "../objects/GameState";
+import { Player } from "../objects/Player";
 import { Tile } from "../objects/Tile";
+
+import { doAction } from "../logic/TheEgg";
 
 import * as Map from "../logic/Map";
 
@@ -15,8 +20,8 @@ import { TileSet } from "../logic/TileSet";
 
 import { fromJS } from "immutable";
 
-const playerTypes = new PlayerTypes();
-const players = playerTypes.getPlayerTypes();
+let currentGameState = null;
+let currentRenderer = null;
 
 const board = Map.makeBoardFromArray([
   [
@@ -64,6 +69,17 @@ const renderMap = [
   [true, true, true, true, true]
 ];
 
+const player = new Player({
+  coords: new Coords({
+    x: 2,
+    y: 2
+  }),
+  ...playerTypes.egg,
+  direction: new Coords({
+    x: 1
+  })
+});
+
 const getScreenSize = () => {
   const { height, width } = Dimensions.get("window");
   return {
@@ -72,23 +88,70 @@ const getScreenSize = () => {
   };
 };
 
+const createGameState = () => {
+  return new GameState({
+    players: [player],
+    board
+  });
+};
+
+// do next move, plop new state on pile, return new state
+const getNewGameState = (
+  gameState: GameState,
+  action: string,
+  timePassed: number
+): GameState => {
+  const newGameState = doAction(gameState, action, timePassed);
+  // this.updateGameState(gameState, newGameState);
+  currentGameState = newGameState;
+  // this.playSounds(gameState, newGameState);
+  return newGameState;
+};
+
+const eventLoop = (time: number, lastTime: number) => {
+  const anim = window.requestAnimationFrame(newTime =>
+    eventLoop(newTime, time)
+  );
+
+  const timePassed = calcTimePassed(time, lastTime);
+
+  // const action = this.getNextAction();
+  const action = "";
+
+  gameCycle(timePassed, action);
+};
+
+const gameCycle = (timePassed: number, action: string) => {
+  const gameState = currentGameState;
+  const nextGameState = getNewGameState(gameState, action, timePassed);
+  const renderer = currentRenderer;
+  renderer.render(nextGameState.board, renderMap, nextGameState.players, 0);
+};
+
+const calcTimePassed = (time: number, lastTime: number): number => {
+  const difference = Math.min(time - lastTime, 20);
+  return difference;
+};
+
 export default class BoardComponent extends React.Component {
   public handleCanvas = canvas => {
     const { screenHeight, screenWidth } = getScreenSize();
     const canvasClass = new CanvasClass(canvas, screenWidth, screenHeight);
     // change to props
-    const size = 4;
+    const size = 5;
     const boardSize = new BoardSize(size);
+    currentGameState = createGameState();
 
     const renderer = new Renderer(
       TileSet.getTiles(),
-      players,
+      playerTypes,
       boardSize,
       canvasClass,
       () => {
+        currentRenderer = renderer;
         canvasClass.wipeCanvas("#000000");
-        renderer.render(board, renderMap, [], 0);
-        console.log("Renderer loaded!");
+        eventLoop(1, 0);
+        // console.log("Renderer loaded!");
       }
     );
   };
